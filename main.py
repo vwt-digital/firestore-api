fddbba49c8b704f471d7ec44565c140bad0cd50f
Jules van Enckevort <j.vanenckevort@vwt.digital>
2020-01-12 15:50:27 +0100

    Remove auth and checks in favor of letting google ESP handle those

--- main.py
+++ main.py
@@ -1,37 +1,15 @@
-import utils
-import config
 import logging
 
 from flask import jsonify
 from google.cloud import firestore_v1
 
 db = firestore_v1.Client()
-secret = utils.decrypt_secret(
-    config.api['project'],
-    config.api['region'],
-    config.api['keyring'],
-    config.api['key'],
-    config.api['secret_base64']
-)
 
 
 def handler(request):
     arguments = dict(request.args)
-    if not arguments.get('apiKey') == secret:
-        return ('Unauthorized!', 403)
-    arguments.pop('apiKey')
-
-    if not request.method == 'GET':
-        return ('Method not allowed!', 405)
-
     path = request.view_args['path']
     collection = path.split('/')[1]
-
-    col_refs = db.collections()
-    collections = [col.id for col in col_refs]
-    if collection not in collections:
-        return ('Not found!', 404)
-
     q = db.collection(collection)
 
     limit = arguments.get('limit', None)
